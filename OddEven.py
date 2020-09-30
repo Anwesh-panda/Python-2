d14a9588d6b7dab2559f369e2fd819978a64b0cf
Anwesh-panda <72140001+Anwesh-panda@users.noreply.github.com>
2020-09-30 18:47:41 +0530

    Update OddEven.py

--- OddEven.py
+++ OddEven.py
@@ -1,5 +1,9 @@
-num = int(input("Enter a number: "))
-if (num % 2) == 0:
-   print("{0} is Even".format(num))
-else:
-   print("{0} is Odd".format(num))
+def oddeven(x):
+       if (number % 2) == 0:
+           print("{0} is Even".format(number))
+       else:
+           print("{0} is Odd".format(number))
+number=int(input("Enter a number: "))
+print(oddeven(number))
+            
+            
